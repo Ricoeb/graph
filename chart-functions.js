68a40440b68cdcf4627f59fa026c28ad9791466f
Ricoeb <46389394+Ricoeb@users.noreply.github.com>
2019-01-05 18:45:48 +0100

    maj v2

--- chart-functions.js
+++ chart-functions.js
@@ -10,7 +10,7 @@
         type: 'line'
     },
     title: {
-        text: 'Product Sales'
+        text: 'Titre du Graph'
     },
     xAxis: {
         categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
@@ -28,13 +28,7 @@
             enableMouseTracking: false
         }
     },
-    series: [{
-        name: 'Tokyo',
-        data: [7.0, 6.9, 9.5, 14.5, 18.4, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
-    }, {
-        name: 'London',
-        data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
-    }]
+    series: seriesTEST
 });
 
 //Sales Overview
